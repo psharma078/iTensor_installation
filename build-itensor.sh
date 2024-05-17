bfbac1b995ee2854a818b9f871c91efdad77de88
psharma078 <108942215+psharma078@users.noreply.github.com>
2024-05-17 14:49:37 -0700

    Update build-itensor.sh

--- build-itensor.sh
+++ build-itensor.sh
@@ -23,4 +23,14 @@ cd ITensor
 cp options.mk.sample options.mk
 vi options.mk
 edit : 
-	PLATFORM=mkl                                                                                                                                                BLAS_LAPACK_LIBFLAGS=-L$(MKLROOT)/lib/intel64 \                                                                                                                                                      -Wl,-rpath,$(MKLROOT)/lib/intel64 \                                                                                                                         -L/opt/intel/tbb/lib/intel64_lin/gcc4.7 \                                                                                                                   -Wl,-rpath,/opt/intel/tbb/lib/intel64_lin/gcc4.7 \                                                                                                          -lmkl_rt -lpthread#/opt/intel/tbb/lib/intel64_lin/gcc4.7/libtbb.so.2 /opt/intel/tbb/lib/intel64_lin/gcc4.7/libtbbmalloc.so.2 -lpthread -lm -ldl                                                                                                                                                                    
+	MKLROOT=/opt/intel/mkl
+	PLATFORM=mkl                                                                                                                                               
+ BLAS_LAPACK_LIBFLAGS=-L$(MKLROOT)/lib/intel64 \                                                                                                                                                  
+ 	-Wl,-rpath,$(MKLROOT)/lib/intel64 \                                                                                                                      
+  	-L/opt/intel/tbb/lib/intel64_lin/gcc4.7 \                                                                                                                 
+  	-Wl,-rpath,/opt/intel/tbb/lib/intel64_lin/gcc4.7 \                                                                                                       
+  	-lmkl_rt -lpthread#/opt/intel/tbb/lib/intel64_lin/gcc4.7/libtbb.so.2 /opt/intel/tbb/lib/intel64_lin/gcc4.7/libtbbmalloc.so.2 -lpthread -lm -ldl       
+
+#add following command to .bashrc
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/psharma/ITensor/lib
+
